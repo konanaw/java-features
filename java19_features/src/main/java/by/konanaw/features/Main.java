cafaf80329a36e945a4e7b0b39f66c8e3103e2ca
konanaw <p033itiff@gmail.com>
2022-12-23 01:07:57 +0300

    Example Project Loom

---
+++ java19_features/src/main/java/by/konanaw/features/Main.java
@@ -0,0 +1,23 @@
+package by.konanaw.features;
+
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    private static StructuredConcurrencyProcessor structuredConcurrencyProcessor;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        System.out.println("Hello world!");
+        System.out.println("¯\\_(ツ)_/¯");
+
+        structuredConcurrencyProcessor = new StructuredConcurrencyProcessor();
+        //
+        structuredConcurrencyProcessor.runSuccess();
+        //
+        System.out.println("***** running with throw *****");
+        //
+        structuredConcurrencyProcessor.runFail();
+
+    }
+}
---
+++ java19_features/src/main/java/by/konanaw/features/StructuredConcurrencyProcessor.java
@@ -0,0 +1,63 @@
+package by.konanaw.features;
+
+import jdk.incubator.concurrent.StructuredTaskScope;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class StructuredConcurrencyProcessor {
+
+    public void runSuccess() throws InterruptedException, ExecutionException {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Future<String> user = scope.fork(() -> findUser());
+            Future<Integer> order = scope.fork(() -> fetchOrder());
+
+            scope.join();           // Join both forks
+            scope.throwIfFailed();  // ... and propagate errors
+
+            Response response = new Response(user.resultNow(), order.resultNow());
+            System.out.println(response);
+
+//            System.out.println(String.format("User %s, order %s", user.resultNow(), order.resultNow()));
+        }
+    }
+
+    public void runFail() throws InterruptedException, ExecutionException {
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Future<String> user = scope.fork(() -> findUserFail());
+            Future<Integer> order = scope.fork(() -> fetchOrder());
+
+            scope.join();           // Join both forks
+            scope.throwIfFailed();  // ... and propagate errors
+
+            Response response = new Response(user.resultNow(), order.resultNow());
+            System.out.println(response);
+
+//            System.out.println(String.format("User %s, order %s", user.resultNow(), order.resultNow()));
+        }
+    }
+
+    private String findUser() throws InterruptedException {
+        System.out.println("findUser started");
+        Thread.sleep(Duration.ofMillis(300));
+        System.out.println("findUser completed");
+        return "Konanaw";
+    }
+
+    private String findUserFail() throws InterruptedException {
+        System.out.println("findUser started");
+        Thread.sleep(Duration.ofMillis(300));
+        throw new RuntimeException("Boom!");
+    }
+
+    private Integer fetchOrder() throws InterruptedException {
+        System.out.println("fetchOrder started");
+        Thread.sleep(Duration.ofMillis(700));
+        System.out.println("fetchOrder completed");
+        return 123;
+    }
+}
+
+record Response(String user, Integer order) {
+}
